73249bb80da3a3b48863b54876c0e25ce5e74ea0
Dev Ask <techdevask@gmail.com>
2025-10-20 13:22:45 +0100

    Create contextInfo.js

---
+++ system/contextInfo.js
@@ -0,0 +1,10 @@
+// ==================== system/contextInfo.js ====================
+export const contextInfo = {
+  forwardingScore: 999,
+  isForwarded: true,
+  forwardedNewsletterMessageInfo: {
+    newsletterJid: '120363330359618597@newsletter',
+    newsletterName: '𝐀𝐒𝐊 𝐓𝐄𝐂𝐇 || 𝐎𝐅𝐅𝐂',
+    serverMessageId: 150
+  }
+};
