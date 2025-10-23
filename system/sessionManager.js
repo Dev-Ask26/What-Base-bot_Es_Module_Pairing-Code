f06ab3b9390ae64054400dc356e0552d05ad661f
Dev Ask <techdevask@gmail.com>
2025-10-23 01:19:02 +0100

    Update sessionManager.js

--- system/sessionManager.js
+++ system/sessionManager.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const configPath = path.join(__dirname, "config.json");
+const configPath = path.join(__dirname,"..", "config.json");
 
 class SessionManager {
   constructor() {
