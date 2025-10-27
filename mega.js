590f2fb023007be8ed065fccca46891670779784
Dev Ask <techdevask@gmail.com>
2025-10-27 23:07:34 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import mega from "megajs";
 
 const auth = {
-    email: '',   // use your real valid mega account email
-    password: '',  // use your real valid mega account password
+    email: 'domkaya4@gmail.com',   // use your real valid mega account email
+    password: 'Kaya2005@',  // use your real valid mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
